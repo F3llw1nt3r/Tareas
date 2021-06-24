65b91000c490079cc9be9ac9e8ee56d74947636e
F3llw1nt3r <75743896+F3llw1nt3r@users.noreply.github.com>
2021-06-23 20:46:19 -0500

    Tarea 2

---
+++ src/Ico/fes/componentes/Computer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ico.fes.componentes;
+
+/**
+ *
+ * @author Abraham
+ */
+public class Computer {
+    public static void main(String[] args){
+   String
+     Computadora compu1= new Computadora("Asus","Atrix"), new Monitor("Acer","FREESYNC",23.6i),
+     new Mouse("Logitech G","Inalambrico","Optico"),new Teclado("Hyper x","Gamer",104f,2i),
+     new CPU("AMD", "Trinity", 4.2i);        
+    }
+}
